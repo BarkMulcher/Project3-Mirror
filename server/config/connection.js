402696e67dee48c34e87c2fd4518af329f25bf47
BarkMulcher <lucas.c.haskell@gmail.com>
2022-12-16 16:56:11 -0500

    production mongo URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/petsDB';
+  process.env.MONGODB_URI || 'mongodb+srv://derekson:p0tato33!@learningcluster.cgwegmn.mongodb.net/?retryWrites=true&w=majority';
 
 connect(connectionString, {
   useNewUrlParser: true,
